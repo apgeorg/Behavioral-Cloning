b0c50361f99d8fbc52fd114e3c397bbb2208da72
Apostolos Georgiadis <apostolos.georgiadis@nfiniity.com>
2017-07-04 17:17:31 +0200

    left right used

--- utils/utils.py
+++ utils/utils.py
@@ -8,21 +8,28 @@ def read_csv(driving_log_csv):
         reader = csv.reader(csvfile)
         return [line for line in reader]
 
-
+def get_image(imgs_path, source_path):
+    filename = source_path.split('/')[-1]
+    return cv2.imread(imgs_path + filename)
+    
 def get_train(IMG_path, driving_log_csv):        
     images = []
     measurements = []
+    correction = 0.2
     lines = read_csv(driving_log_csv)
     for line in lines[1:]:
-        src_path = line[0]
-        filename = src_path.split('/')[-1]
-        cur_path =  IMG_path + filename
-        img = cv2.imread(cur_path)
+        img = get_image(IMG_path, line[0])
+        img_left = get_image(IMG_path, line[1])
+        img_right = get_image(IMG_path, line[2])
         measurement = float(line[3])
         images.append(img)
-        images.append(cv2.flip(img, 1))
         measurements.append(measurement)
-        measurements.append(measurement * -1.)
+        images.append(cv2.flip(img, 1))
+        measurements.append(-1.*measurement)
+        images.append(img_left)
+        measurements.append(measurement+correction)
+        images.append(img_right)
+        measurements.append(measurement-correction)
     return np.array(images), np.array(measurements)
 
 """
